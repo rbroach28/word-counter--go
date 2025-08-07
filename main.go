131b83fc630422b142bc0fd9a23ab20924470248
Robby Broach <rbroach@trekk.com>
2025-08-07 16:27:26 -0500

    Word count algorithm.

--- main.go
+++ main.go
@@ -1,5 +1,23 @@
 package main
 
+import (
+	"fmt"
+	"os"
+)
+
 func main() {
+	data, _ := os.ReadFile("words.txt")
+	
+	wordCount := 0
+
+	for _, x := range data {
+		if x ==  ' ' {
+			wordCount++
+		}	
+	}
+
+	wordCount++ // for the last word
+
+	fmt.Println(wordCount)
 
 }
